789fa292d5d0b92a6f177925e379c4ca58715a7e
Francisco G <fsalvador23@gmail.com>
2019-11-14 11:13:04 +0100

    First commit

---
+++ app.py
@@ -0,0 +1,15 @@
+#coding: utf-8
+
+from flask import Flask, render_template, jsonify
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return jsonify(
+        username="rest api ready",
+        email="rest api ready",
+        id="rest api ready"
+    )
+
+if __name__ == '__main__':
+    app.run(debug=False,host='0.0.0.0')
